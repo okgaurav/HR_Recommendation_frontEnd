0f1ed02f319503fe0487877ab3cfa512a51c2596
GUPTA Ritik <ritik.gupta@soprabanking.com>
2022-07-20 18:33:54 +0530

    Draft API in progress

---
+++ src/main/java/com/sbs/hrRecommendation/controllers/recommendationFeedController.java
@@ -0,0 +1,58 @@
+package com.sbs.hrRecommendation.controllers;
+
+import com.sbs.hrRecommendation.models.recommendation;
+import com.sbs.hrRecommendation.repositories.recommendationRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/api/recommendations")
+public class recommendationFeedController {
+
+    @Autowired
+    private recommendationRepository recRepository;
+
+    @GetMapping
+    public List<recommendation> list() {
+        return recRepository.findAll();
+    }
+    //To get data of a user using a particular id
+    @GetMapping
+    @RequestMapping("{id}")
+    public recommendation get(@PathVariable Long id){
+        return recRepository.getOne(id);
+    }
+    //push data into DB
+    @PostMapping
+    public recommendation create(@RequestBody final recommendation Recommendation) {
+        Recommendation.setMyStatus(recommendation.status.SENT);
+        return recRepository.saveAndFlush(Recommendation);
+    }
+
+    //Used to update the record.
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public recommendation update(@PathVariable Long id, @RequestBody recommendation Recommendation) throws Exception {
+        // DRAFT
+        recommendation existingRecommendation = recRepository.getReferenceById(id);
+        Long authorId =  existingRecommendation.getUserId();
+        Long requestUserId = Recommendation.getUserId();
+        recommendation.status recommendationStatus = existingRecommendation.getMyStatus();
+        System.out.println(Objects.equals(recommendationStatus, recommendation.status.DRAFT) + "#######");
+        if(!Objects.equals(authorId, requestUserId) || !Objects.equals(recommendationStatus, recommendation.status.DRAFT)){
+            // User is not authorised to update draft
+             System.out.println("Unauth access");
+        }
+        else{
+            existingRecommendation.setSubject(Recommendation.getSubject());
+            existingRecommendation.setDescription(Recommendation.getDescription());
+            existingRecommendation.setIsPrivate(Recommendation.getIsPrivate());
+
+            BeanUtils.copyProperties(Recommendation, existingRecommendation, "recommendationId");
+        }
+        return recRepository.saveAndFlush(existingRecommendation);
+    }
+}
---
+++ src/main/java/com/sbs/hrRecommendation/models/recommendation.java
@@ -0,0 +1,123 @@
+package com.sbs.hrRecommendation.models;
+
+
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+@Entity(name="recommendations")
+public class recommendation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="recommendation_id")
+    private Long recommendationId;
+    @Column(name="user_id")
+    private Long userId;
+    @Column(name="subject")
+    private String subject;
+    @Column(name="description")
+    private String description;
+    @Column(name="is_private")
+    public boolean isPrivate;
+    @Column(name="created_at")
+    private LocalDateTime createdAt=LocalDateTime.now();
+    @Column(name="modified_at")
+    private LocalDateTime modifiedAt=LocalDateTime.now();
+    @Column(name="status")
+    @Enumerated(EnumType.STRING)
+    private status myStatus;
+
+    public status getMyStatus() {
+        return myStatus;
+    }
+
+    public void setMyStatus(status myStatus) {
+        this.myStatus = myStatus;
+    }
+
+//    public userProfile getUsers() {
+//        return users;
+//    }
+//
+//    public void setUsers(userProfile users) {
+//        this.users = users;
+//    }
+
+    public enum status{
+        DRAFT,
+        PENDING,
+        CHANGES_REQUESTED,
+        APPROVED,
+        DECLINED,
+        SENT, ARCHIVED
+    }
+    @ManyToOne
+    private userProfile users;
+
+
+    public Long getRecommendationId() {
+        return recommendationId;
+    }
+
+    public void setRecommendationId(Long recommendationId) {
+        this.recommendationId = recommendationId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean getIsPrivate() {
+        return isPrivate;
+    }
+
+    public void setIsPrivate(boolean aPrivate) {
+        isPrivate = aPrivate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+    //    public String getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(String status) {
+//        this.status = status;
+//    }
+}
+
