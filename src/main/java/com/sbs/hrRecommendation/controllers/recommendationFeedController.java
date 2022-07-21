3607a7610b5bef53977b6b442f0ec34d7fd45277
GUPTA Ritik <ritik.gupta@soprabanking.com>
2022-07-21 11:30:45 +0530

    Draft API completed, bug removed

--- src/main/java/com/sbs/hrRecommendation/controllers/recommendationFeedController.java
+++ src/main/java/com/sbs/hrRecommendation/controllers/recommendationFeedController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
@@ -34,24 +35,25 @@ public class recommendationFeedController {
     }
 
     //Used to update the record.
-    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
-    public recommendation update(@PathVariable Long id, @RequestBody recommendation Recommendation) throws Exception {
+    @PutMapping("{id}")
+    public recommendation updateRecommendation(@PathVariable Long id, @RequestBody recommendation Recommendation) throws Exception {
         recommendation existingRecommendation = recRepository.getReferenceById(id);
         Long authorId =  existingRecommendation.getUserId();
         Long requestUserId = Recommendation.getUserId();
         recommendation.status existingRecommendationStatus = existingRecommendation.getMyStatus();
+        LocalDateTime lt = LocalDateTime.now();
 
-        // Author should be same as request owner
-        // Recommendation should be in DRAFT state
-        if(!Objects.equals(authorId, requestUserId) || !Objects.equals(existingRecommendationStatus, recommendation.status.DRAFT)){
-            // User is not authorised to update draft
-             System.out.println("Unauthorised access or Status is not DRAFT");
+        // DRAFT : Author should be same as request owner and Recommendation should be in DRAFT state
+        if(Objects.equals(authorId, requestUserId) && Objects.equals(existingRecommendationStatus, recommendation.status.DRAFT)){
+            existingRecommendation.setSubject(Recommendation.getSubject());
+            existingRecommendation.setDescription(Recommendation.getDescription());
+            existingRecommendation.setIsPrivate(Recommendation.getIsPrivate());
+            existingRecommendation.setModifiedAt(lt);
             return recRepository.saveAndFlush(existingRecommendation);
         }
-        existingRecommendation.setSubject(Recommendation.getSubject());
-        existingRecommendation.setDescription(Recommendation.getDescription());
-        existingRecommendation.setIsPrivate(Recommendation.getIsPrivate());
-        BeanUtils.copyProperties(Recommendation, existingRecommendation, "recommendationId");
+
+        // User is not authorised to update draft
+        System.out.println("Unauthorised access or Status is not DRAFT");
         return recRepository.saveAndFlush(existingRecommendation);
     }
 }
