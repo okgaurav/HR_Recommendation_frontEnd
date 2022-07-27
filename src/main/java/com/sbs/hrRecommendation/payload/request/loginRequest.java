b0933810876152d3ad8aebeae456f17424cc6dc3
TRIPATHI Nikhil <nikhil.tripathi@soprabanking.com>
2022-07-27 16:27:02 +0530

    implement JWT

--- src/main/java/com/sbs/hrRecommendation/payload/request/loginRequest.java
+++ src/main/java/com/sbs/hrRecommendation/payload/request/loginRequest.java
@@ -1,4 +1,29 @@
 package com.sbs.hrRecommendation.payload.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Email;
+
 public class loginRequest {
+    @NotBlank
+    @Email
+    private String email_id;
+
+    @NotBlank
+    private String password;
+
+    public String getEmail() {
+        return email_id;
+    }
+
+    public void setEmail(String email_id) {
+        this.email_id = email_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
